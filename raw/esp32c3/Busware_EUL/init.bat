cab0b40406b4b3f9706e9455d38f63b54d59aa7d
Dirk Tostmann <tostmann@busware.de>
2024-01-01 14:59:48 +0100

    fix

--- raw/esp32c3/Busware_EUL/init.bat
+++ raw/esp32c3/Busware_EUL/init.bat
@@ -1,5 +1,4 @@
 Template {"NAME":"Busware EUL","GPIO":[1,1,1,3840,544,1,1,1,1,1,1,0,0,0,0,0,0,0,1,1,5504,5472],"FLAG":0,"BASE":1}
 Module 0
-
-rule1 on system#boot do backlog TCPBaudrate 57600 ; TCPConfig 8N1 ; TCPStart 2323 endon
+rule1 on system#boot do backlog TCPBaudrate 57600; TCPConfig 8N1; TCPStart 2323 endon
 
