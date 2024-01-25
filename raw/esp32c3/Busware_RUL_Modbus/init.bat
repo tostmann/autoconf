de8811a8c5ce54018cde4111feb0da323ef2fe38
Dirk Tostmann <tostmann@busware.de>
2024-01-25 23:19:23 +0100

    added autoconf for Modbus on RUL gadget

---
+++ raw/esp32c3/Busware_RUL_Modbus/init.bat
@@ -0,0 +1,4 @@
+Template {"NAME":"Busware RUL Modbus","GPIO":[0,0,0,9952,288,1,1,1,1,1,1,0,0,0,0,0,0,0,1,1,9440,9408],"FLAG":0,"BASE":1}
+Module 0
+rule1 on system#boot do backlog ModbusBaudrate 9600; ModbusSerialConfig 8N1 endon
+rule1 1
