51ab8edc3cc523a056a692e89d031daa31dd0a08
Dirk Tostmann <tostmann@busware.de>
2024-01-30 23:37:59 +0100

    added ZUL and CUN

---
+++ raw/esp32c3/Busware_ZUL/init.bat
@@ -0,0 +1,4 @@
+Template {"NAME":"Busware ZUL","GPIO":[1,1,1,3840,544,1,1,1,1,1,1,0,0,0,0,0,0,0,1,1,5504,5472],"FLAG":0,"BASE":1}
+Module 0
+rule1 on system#boot do backlog TCPBaudrate 115200; TCPConfig 8N1; TCPStart 2326 endon
+rule1 1
